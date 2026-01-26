a71540dcaf3cb58ee6257b1bd2d6f737a791db24
yasmineCH03 <yasmine.chebbi@esprit.tn>
2026-01-26 11:54:13 +0100

    creation de la classe championnat

---
+++ src/main/java/tn/esprit/championnat/Entity/Categorie.java
@@ -0,0 +1,6 @@
+package tn.esprit.championnat.Entity;
+
+public enum Categorie {
+    FORMULA1,
+    MOTOGP
+}
---
+++ src/main/java/tn/esprit/championnat/Entity/championnat.java
@@ -0,0 +1,10 @@
+package tn.esprit.championnat.Entity;
+import tn.esprit.championnat.*;
+public class championnat {
+
+    private long idChampionnat;
+    private String libelleC;
+    private int annee;
+    private Categorie categorie;
+
+}
