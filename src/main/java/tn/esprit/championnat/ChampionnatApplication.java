5c54b0b0cd914f403fb13e345698df8a74271d9b
yasmineCH03 <yasmine.chebbi@esprit.tn>
2026-01-26 10:28:26 +0100

    1er commit projet spring - atelier 1

---
+++ src/main/java/tn/esprit/championnat/ChampionnatApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.championnat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChampionnatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChampionnatApplication.class, args);
+    }
+
+}
